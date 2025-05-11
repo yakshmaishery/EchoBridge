163447f41755fa2be8bc5a11219e0c91c96b5300
yakshmaishery <yakshmaishery@users.noreply.github.com>
2025-05-11 20:25:06 +0530

    few changes

--- src/lib/UserDashboardWindows/SessionManagement.svelte
+++ src/lib/UserDashboardWindows/SessionManagement.svelte
@@ -12,7 +12,7 @@
    <form method="post"use:enhance>
       <div class="sessionmanagercontroler">
          <input type="text" name="UserIDDeafult" on:input={validate} placeholder="Enter Default UserID..." autocomplete="off" maxlength="4" minlength="4" class="border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-30 rounded-md border px-3 py-2 text-base file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm" />
-         <button class="bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 rounded-md" formaction="?/SetAnotherIDDefault" disabled={!isValid}>Save</button>
+         <button class="bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 rounded-md" formaction="?/SetAnotherIDDefault">Save</button>
          <button class="bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 rounded-md" formaction="?/RemoveanotherIDDefault">Clear</button>
       </div>
    </form>
